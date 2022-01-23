b1c95683bb16a20561207d9336d623cfc9de5cc5
Rekurapro <98193018+Rekurapro@users.noreply.github.com>
2022-01-23 19:06:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import youtube
 import requests
 import random
 import telebot
-import config
 from telebot import types
+
+
 headerss = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Encoding': 'gzip, deflate, br',
